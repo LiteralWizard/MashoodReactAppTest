a82c413f5922a481ae71e8a5b23a02d412cfed79
Mashood Ur Rehman <mashoodrehman97@gmail.com>
2020-04-09 14:23:41 +0500

    Add files via upload

---
+++ Mashood_i160063.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+class Mashood extends Component {
+    render() {
+      return (
+        <div style={{fontFamily: ['Roboto', 'sans-serif']}}>
+            <h1>Mashood Ur Rehman</h1>
+            <h2>i160063</h2>
+            <h2>IBC Quiz 5</h2>
+
+            <br></br>
+            <h4>Instead of an article, I have one of my favourite poems</h4>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/2/26/Forest_Walk.jpg'/>
+            <br></br>
+            <h2>The Road Not Taken</h2>
+            <h4>By Robert Frost</h4>
+
+            <div style={{fontFamily: ['Georgia', 'serif']}}>
+                <p>Two roads diverged in a yellow wood,<br/>
+                And sorry I could not travel both<br/>
+                And be one traveler, long I stood<br/>
+                And looked down one as far as I could<br/>
+                To where it bent in the undergrowth;</p>
+
+                <p>Then took the other, as just as fair,<br/>
+                And having perhaps the better claim,<br/>
+                Because it was grassy and wanted wear;<br/>
+                Though as for that the passing there<br/>
+                Had worn them really about the same,</p>
+
+                <p>And both that morning equally lay<br/>
+                In leaves no step had trodden black.<br/>
+                Oh, I kept the first for another day!<br/>
+                Yet knowing how way leads on to way,<br/>
+                I doubted if I should ever come back.</p>
+
+                <p>I shall be telling this with a sigh<br/>
+                Somewhere ages and ages hence:<br/>
+                Two roads diverged in a wood, and I—<br/>
+                I took the one less traveled by,<br/>
+                And that has made all the difference.</p>
+            </div>
+        </div>
+      ); }
+  }
+  
+  export default Mashood;
